b5644344a4f8c00bc87f188dc12fe43c3aff3a3e
Semere <zemosemere@gmail.com>
2021-07-26 12:52:57 -0400

    All is done

--- src/ResetPractice.java
+++ src/ResetPractice.java
@@ -3,4 +3,8 @@ public class ResetPractice {
     //part 1 is done
     //part 2 is done
     //part 3 is done
+    //part 4 is done
+    //part 5 is done
+    // Mixed/soft reset remove the commit from history but keep local.....................
+    // Did part 4 part 5 and i,m done in one commit
 }
