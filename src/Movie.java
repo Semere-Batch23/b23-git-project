f6e1f5fddc8901b206858c670c28b8f24d559445
Semere <zemosemere@gmail.com>
2021-07-29 15:30:12 -0400

    untill 40 mints

--- src/Movie.java
+++ src/Movie.java
@@ -1,8 +1,9 @@
 public class Movie {
 
     public static void main(String[] args) {
-        //watching 10 more minutes
-        //until 25 mints
+        //watching 25 more minutes
+        //until 30 mints
+        //intill 40 mints
 
     }
 }
